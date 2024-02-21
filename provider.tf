282a2f53826c6ebbdfdc9205180e7b3ea360cd79
sagar <150423510+Sagar0546@users.noreply.github.com>
2024-02-21 19:19:39 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,3 @@
-   provider "aws" {
-region = "us-east-2"
-aws_access_key_id = AKIAZI2LHOSUH44CE2VC
-aws_secret_access_key = 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
+provider "aws" {
+  region = "us-east-2"
 }
