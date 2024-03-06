ccc708b42dde9145d3efe2cac9fa167394775ee8
Singuluri pavan kumar <150420006+PavanGitPro@users.noreply.github.com>
2024-03-06 12:07:14 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,14 +1,14 @@
-resource "aws_instance" "WORDPRESS" {
+resource "aws_instance" "python" {
   ami           = "ami-0cf7b2f456cd5efd4"
   instance_type = "t2.micro"
 
-  key_name                    = "TTT"
+  key_name                    = "pyt-1"
   vpc_security_group_ids      = [aws_security_group.sg1.id]
   subnet_id                   = aws_subnet.publicsubnet1.id
   associate_public_ip_address = true
   user_data                   = file("data.sh")
   tags = {
-    Name = "wordpress"
+    Name = "python"
   }
 }
 
