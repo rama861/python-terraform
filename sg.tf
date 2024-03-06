0fbef4199885f39aaef6df094a1f3b73d1d417f7
rama861 <161824892+rama861@users.noreply.github.com>
2024-03-06 08:15:40 -0600

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -7,19 +7,20 @@ resource "aws_security_group" "sg1" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
- ingress {
-    from_port   = 22
-    to_port     = 22
+  ingress {
+    from_port   = 0
+    to_port     = 0
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-egress {
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags = {
     Name = "pysg"
   }
