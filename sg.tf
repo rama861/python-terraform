8c6fbc83e450459f80994ec0807b579510c0124f
rama861 <161824892+rama861@users.noreply.github.com>
2024-03-06 06:44:52 -0600

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,24 +1,20 @@
 resource "aws_security_group" "sg1" {
   vpc_id = aws_vpc.demovpc.id
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8000
+    to_port     = 8000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  egress {
+
+egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
