81c3d9a2997d2335a0477c032c7b99687f6c72a1
Singuluri pavan kumar <150420006+PavanGitPro@users.noreply.github.com>
2024-03-06 14:38:05 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 sudo yum update -y
 sudo yum install git -y
-git clone 
+git clone https://github.com/PavanGitPro/carprediction.git
+cd carprediction
+pip3 install -r requirements.txt
+screen -m -d python3 app.py
