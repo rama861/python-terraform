38ddc4ee0455ff24dd09a3761e19021ccd4f11da
rama861 <161824892+rama861@users.noreply.github.com>
2024-03-06 11:27:37 -0600

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,13 +1,12 @@
 resource "aws_instance" "python" {
-  ami           = "ami-07761f3ae34c4478d"
-  instance_type = "t2.micro"
-
-  key_name                    = "shiva"
-  vpc_security_group_ids      = [aws_security_group.sg1.id]
-  subnet_id                   = aws_subnet.publicsubnet1.id
-  associate_public_ip_address = true
-  user_data                   = file("data.sh")
-  tags = {
+ami           = "ami-07761f3ae34c4478d"
+instance_type = "t2.micro"
+key_name                    = "shiva"
+vpc_security_group_ids      = [aws_security_group.sg1.id]
+subnet_id                   = aws_subnet.publicsubnet1.id
+associate_public_ip_address = true
+ user_data                   = file("data.sh")
+tags = {
     Name = "python"
   }
 }
