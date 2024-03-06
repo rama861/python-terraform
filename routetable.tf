0a67c166f1852b16e0161740334e4889a125199c
rama861 <161824892+rama861@users.noreply.github.com>
2024-03-06 08:14:32 -0600

    Update routetable.tf

--- routetable.tf
+++ routetable.tf
@@ -2,8 +2,8 @@ resource "aws_route_table" "route1" {
   vpc_id = aws_vpc.demovpc.id
 
    route {
-      cidr_block = var.anywhere_cidr
-      gateway_id = aws_internet_gateway.demoigw.id
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.igpy.id
     }
 
   tags = {
